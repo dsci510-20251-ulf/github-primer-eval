3f1f02d5ac730983c9cd675d89bd966898dc5240
OmkarMetri <metriomkar56@gmail.com>
2025-02-02 10:23:02 -0800

    open cases

--- tests/test_open.py
+++ tests/test_open.py
@@ -1,54 +1,27 @@
 import pytest
-import math
 from lab_primer import number_addition
 
 
 @pytest.mark.timeout(0.3)
-def test_number_addition_positive_integers():
+def test_number_addition_1():
     assert number_addition(2, 3) == 5
 
 
 @pytest.mark.timeout(0.3)
-def test_number_addition_negative_integers():
+def test_number_addition_2():
     assert number_addition(-2, -3) == -5
 
 
 @pytest.mark.timeout(0.3)
-def test_number_addition_mixed_integers():
+def test_number_addition_3():
     assert number_addition(-2, 3) == 1
 
 
 @pytest.mark.timeout(0.3)
-def test_number_addition_floats():
+def test_number_addition_4():
     assert number_addition(2.5, 3.1) == 5.6
 
 
 @pytest.mark.timeout(0.3)
-def test_number_addition_float_integer():
-    assert number_addition(1, 2.6) == 3.6
-
-
-@pytest.mark.timeout(0.3)
-def test_number_addition_failure():
-    # purposely failed for github classroom demo
-    assert number_addition(9999, -9999) == -1
-
-
-@pytest.mark.timeout(0.3)
-def test_number_addition_one_input_zero():
-    assert number_addition(-3, 0) == -3
-
-
-@pytest.mark.timeout(0.3)
-def test_number_addition_small_numbers():
-    assert number_addition(1e-10, 1e-10) == 2e-10
-
-
-@pytest.mark.timeout(0.3)
-def test_number_addition_nan():
-    assert math.isnan(number_addition(math.nan, 1))
-
-
-@pytest.mark.timeout(0.3)
-def test_number_addition_minus_inf_plus_inf():
-    assert math.isnan(number_addition(-math.inf, math.inf))
+def test_number_addition():
+    assert number_addition(9999, -9999) == 0
